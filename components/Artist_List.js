0df1a1761611f4d3380b26fd7ca51089a4643280
bbakercello <bbakercello@gmail.com>
2022-10-10 14:23:56 -0400

    data is rendering on frontend successfully

--- components/Artist_List.js
+++ components/Artist_List.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Artist_List = (props) => {
-    // console.log(props.HEROKU)
-  return (
-    <div>
-        
-        <a href={props.HEROKU}>Heroku</a>
-    </div>
-  )
+
+    const url =`${props.HEROKU}`+ 'artists'
+    console.log(url)
+    
+    const [artist, setArtist] = useState([])
+    
+    const getArtists = async () => {
+        const response = await fetch(url)
+        const data = await response.json();
+        setArtist(data)
+    }
+
+    useEffect(()=> {
+        getArtists();
+    },[]);
+
+    const loaded = () => {
+    
+    return (
+        <div>
+            {artist.map((artist,index)=> {
+                return(
+                    <div>{artist.name}</div>
+            )})}
+        </div>
+    )
+    }
+
+    const loading = () => {
+        return(<h1>Loading...</h1>)
+    }
+
+    return artist ? loaded() : loading();
+
 }
 
 export default Artist_List
