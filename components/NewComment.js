4dabbabeee1dd47dfc2351eedc620798785c6e7a
bbakercello <bbakercello@gmail.com>
2022-10-12 18:44:24 -0400

    handlechange written

--- components/NewComment.js
+++ components/NewComment.js
@@ -6,3 +6,32 @@ const NewComment = (props) => {
       user: '',
       post: ''
     })}
+
+    const handleChangeComment = (e) => {
+        setNewComment({...newComment, [e.target.name]: e.target.value})
+      }
+    
+      const handleSubmit  = async (e) => {
+        e.preventDefault()
+        try{
+          const data = {...newComment, user: `${props.session}`, timestamp: new Date()}
+          const updateComment = await fetch(url, {
+            method: "PUT",
+            body: JSON.stringify(data),
+            headers: {
+              "content-type": "application/JSON"
+            }
+          })
+          
+          props.getPosts();
+          setNewComment({
+            user: '',
+            post: ''
+          })
+        }
+        catch(err){
+          // console.log(err)
+        }
+      }
+
+      export default NewComment
