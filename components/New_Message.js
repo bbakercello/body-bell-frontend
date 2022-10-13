1dea3e0205d5b1f2f650a0517b9dc800f0a4b504
bbakercello <bbakercello@gmail.com>
2022-10-13 07:22:52 -0400

    refactoring form for nextjs

--- components/New_Message.js
+++ components/New_Message.js
@@ -3,67 +3,84 @@ import Collapsible from 'react-collapsible'
 
 
 const New_Message = (props) => {
-
-
+  const mongoId = props.id
   const HEROKU = process.env.NEXT_PUBLIC_BACKEND
 
-  const [newMessage, setNewMessage] = useState ({
-    message: '',
-    artist: ''
-  })}
+  // const [newMessage, setMessage] = useState ({
+  //   message: '',
+  //   artist: ''
+  // })}
 
-  const handleChangeMessage = (e) => {
-        setNewMessage({...newMessage, [e.target.name]: e.target.value})
-    }
+  // const handleChangeMessage = (e) => {
+  //       setMessage({...newMessage, [e.target.name]: e.target.value})
+  //   }
     
-  const handleSubmit  = async (e) => {
+  // const handleSubmit  = async (e) => {
     const url = `${HEROKU}`+ 'messages'
 
-    e.preventDefault()
-    try{
-      const data = {...newMessage}
-      const updateMessage = await fetch(url, {
-        method: "PUT",
-        body: JSON.stringify(data),
-        headers: {
-          "content-type": "application/JSON"
-        }
-      })
+    // e.preventDefault()
+    // try{
+    //   const data = {...newMessage}
+    //   const updateMessage = await fetch(url, {
+    //     method: "PUT",
+    //     body: JSON.stringify(data),
+    //     headers: {
+    //       "content-type": "application/JSON"
+    //     }
+    //   })
           
-      props.getArtist();
-      setNewMessage({
-        message: '',
-        artist: ''
-      })
-    }
-    catch(err){
-      console.log(err)
-    }
+    //   props.getArtist();
+    //   console.log(props.getArtist)
+    //   setMessage({
+    //     message: '',
+    //     artist: ''
+    //   })
+    // }
+    // catch(err){
+    //   console.log(err)
+    // }
 
   return (
-    <section className="collapsible">
-    <Collapsible 
-    trigger = 'new message'>
-      {/* <form onSubmit={handleSubmit}>
+    <form action={url} method="post">
+      <label for="message">Message</label>
       <input
       type="text"
-      name="post"
-      value={newMessage.message}
-      onChange={handleChangeMessage}
+      name="message"
       />
       <input
       type="hidden"
+      value={props.id}
       name="artist"
-      value = {props.mongoId}
       />
-     <input
-      type="submit"
-      name="newMessage"
-       />
-      </form> */}
-      <div>hello</div>
-    </Collapsible>
-    </section>
+      <button type="submit">Submit</button>
+    </form>
   )
 } 
 export default New_Message
+
+
+
+
+{/* <section className="collapsible">
+<Collapsible 
+trigger = 'new message'>
+  <form onSubmit={handleSubmit}>
+  <input
+  type="text"
+  name="post"
+  value={newMessage.message}
+  onChange={handleChangeMessage}
+  />
+  <input
+  type="hidden"
+  name="artist"
+  value = {props.mongoId}
+  />
+ <input
+  type="submit"
+  name="newMessage"
+   />
+  </form>
+  <div>hello</div>
+</Collapsible> */}
+// </section>
