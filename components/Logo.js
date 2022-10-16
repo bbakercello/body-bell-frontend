f29a95c4d50c0b55f5c88b034893756a032c7dc6
bbakercello <bbakercello@gmail.com>
2022-10-16 12:31:55 -0400

    working on email form

--- components/Logo.js
+++ components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = (props) => {
   return (
     <>
-    <nav className='p-5 flex items-left'><img className='w-1/2 rounded-full drop-shadow-lg' src='https://i.imgur.com/hdOhoXL.jpg' alt="Logo"></img></nav>
+    <span className='p-5 flex items-left'><img className='w-1/2 rounded-full drop-shadow-lg' src='https://i.imgur.com/hdOhoXL.jpg' alt="Logo"></img></span>
     </>
   )
 }
